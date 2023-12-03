581a9e22140c669f5f641a1c2403de7977baa935
eisuke-uefuji <145823075+eisuke-uefuji@users.noreply.github.com>
2023-12-04 08:16:23 +0900

    passwordのFakerの設定修正

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :user do
     nickname {Faker::Name.last_name}
     email {Faker::Internet.email}
-    password {Faker::Internet.password(min_length: 6)}
+    password {'1a' + Faker::Internet.password(min_length: 6)}
     password_confirmation {password}
     familyname_kanji {"上藤"}
     firstname_kanji {"英資"}
