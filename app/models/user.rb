014f828f64ffd0429668d1c890a1e979c2dc69e5
eisuke-uefuji <145823075+eisuke-uefuji@users.noreply.github.com>
2023-12-03 10:50:32 +0900

    トップページ　新規投稿ページ、ログインページへのリンク修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  validates :familyname_kana, presence: true, format: { with: /\A[\p{Katakana}\p{Blank}ー－]+\z/}
+  validates :firstname_kana, presence: true, format: { with: /\A[\p{Katakana}\p{Blank}ー－]+\z/}
 end
