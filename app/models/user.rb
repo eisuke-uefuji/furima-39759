cdc0dd602f378080591930b83dd2e871dcfac5c0
eisuke-uefuji <145823075+eisuke-uefuji@users.noreply.github.com>
2023-12-03 20:37:13 +0900

    テストコードの作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,9 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   validates :familyname_kana, presence: true, format: { with: /\A[\p{Katakana}\p{Blank}ー－]+\z/}
   validates :firstname_kana, presence: true, format: { with: /\A[\p{Katakana}\p{Blank}ー－]+\z/}
+  validates :birthdate, presence: true
+  validates :nickname, presence: true 
+  validates :familyname_kanji, presence: true
+  validates :firstname_kanji, presence: true
+  validates :password, presence: true, format: { with: /\A(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]+\z/, message: 'は半角英数字混合で入力してください' }
 end
