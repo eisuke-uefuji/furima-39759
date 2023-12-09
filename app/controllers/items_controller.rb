e7824c9e3c73666587c6ddde94520393489c6e92
eisuke-uefuji <145823075+eisuke-uefuji@users.noreply.github.com>
2023-12-09 18:05:50 +0900

    商品情報入力フォームからのレコード保存まで

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -7,10 +7,17 @@ class ItemsController < ApplicationController
   end
 
   def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   private
   def item_params
-    params.require(:item).permit(:name, :item_summary).merge(user_id: current_user.id)
+    params.require(:item).permit(:name, :item_summary, :price, :category_id, :condition_id, :delivery_charge_id, :shipping_span_id, :prefecture_id, :image).merge(user_id: current_user.id)
   end
+
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,8 +1,15 @@
 class Item < ApplicationRecord
-  
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :prefecture
+
 
   belongs_to :user
   has_one_attached :image
   has_one :purchase
+
+  validates :name, :item_summary, :price, presence: true
+  validates :category_id, :condition_id, :delivery_charge_id, :shipping_span_id, :prefecture_id, numericality: { other_than: 1 ,message: "can`t be blank"}
 
 end
