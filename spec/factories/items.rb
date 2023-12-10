f0790d0803e39a4e76ec4968a1fa3adbbf622916
eisuke-uefuji <145823075+eisuke-uefuji@users.noreply.github.com>
2023-12-10 14:38:44 +0900

    テストコードの記載

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -3,12 +3,17 @@ FactoryBot.define do
     name  { Faker::Commerce.product_name }
     item_summary { Faker::Lorem.sentence }
     price { rand(300..9999999)}
-    image { Faker::Lorem.sentence }
     association :user
-    association :category
-    association :condition
-    association :delivery_charge
-    association :shipping_span
-    association :prefecture 
+    category_id { rand(2..Category.count) }
+    condition_id { rand(2..Condition.count) }
+    delivery_charge_id { rand(2..DeliveryCharge.count) }
+    shipping_span_id { rand(2..ShippingSpan.count) }
+    prefecture_id { rand(2..Prefecture.count) }
+
+    after(:build) do |item|
+      image_path = '/home/eisukeuefuji/projects/furima-39759/app/assets/images/card-visa.gif'
+      item.image.attach(io: File.open(image_path), filename: 'card-visa.gif', content_type: 'image/gif')
+    end
+
   end
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -10,7 +10,43 @@ RSpec.describe Item, type: :item do
         expect(@item).to be_valid
       end
     end
-    context '新規登録できない場合' do
+    context '商品の新規登録できない場合' do
+      it'商品名が空では登録できない' do
+        @item.name = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Name 商品名を入力してください")
+      end
+      it'価格が空では登録できない' do
+        @item.price = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price 価格を入力してください")
+      end
+      it'カテゴリが空では登録できない' do
+        @item.category_id = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Category can`t be blank")
+      end
+      it'conditionが空では登録できない' do
+        @item.condition_id = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Condition can`t be blank")
+      end
+      it'delivery_chargeが空では登録できない' do
+        @item.delivery_charge_id = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Delivery charge can`t be blank")
+      end
+      it'shipping_spanが空では登録できない' do
+        @item.shipping_span_id = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Shipping span can`t be blank")
+      end
+      it'imageが空では登録できない' do
+        @item.image = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Image 画像をアップロードしてください")
+      end
+
     end
 
   end
