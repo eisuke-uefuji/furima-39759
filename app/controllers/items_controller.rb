d46317444d45959f12510e2f55d7ebd8075a7787
eisuke-uefuji <145823075+eisuke-uefuji@users.noreply.github.com>
2023-12-15 20:52:08 +0900

    レビューの修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -19,9 +19,10 @@ class ItemsController < ApplicationController
     end
   end
 
-  def show
-    @items = Item.find(params[:id])
-  end
+  
+  #def show
+  #  @items = Item.find(params[:id])
+  #end
 
   private
 
