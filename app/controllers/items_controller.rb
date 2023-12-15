8ed829e93d7af0bf7ab5fdfb178a2ed947cd2f25
eisuke-uefuji <145823075+eisuke-uefuji@users.noreply.github.com>
2023-12-15 20:12:49 +0900

    商品一覧の表示

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,6 +2,8 @@ class ItemsController < ApplicationController
   before_action :move_to_index, except: [:index]
 
   def index
+    @items = Item.all.order("created_at DESC")
+    @delivery_charges = DeliveryCharge.all
   end
 
   def new
@@ -15,6 +17,10 @@ class ItemsController < ApplicationController
     else
       render :new, status: :unprocessable_entity
     end
+  end
+
+  def show
+    @items = Item.find(params[:id])
   end
 
   private
